4191308a3d4786adf9de6c2f0faeef44a0075477
Alvaro Fernandez Lopez <alvaro.fernandez223@educa.madrid.org>
2024-09-27 17:19:48 +0200

    Viernes

--- org.streams/src/main/java/ejercicioStreams/org/streams/App.java
+++ org.streams/src/main/java/ejercicioStreams/org/streams/App.java
@@ -7,19 +7,11 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashSet;
+import java.util.DoubleSummaryStatistics;
 import java.util.List;
 import java.util.Set;
 import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-import org.vb.ejercicioRepaso.Alumno;
-
 import daw.com.Pantalla;
 import daw.com.Teclado;
 
@@ -57,7 +49,7 @@ public class App {
 
 		alumnos.stream()
 		.filter(Alumno::isRepetidor) //Quitar no repetidores   
-		.filter(a -> a.getNota() >= 5) //Quitar no aprobados 
+		.filter(Alumno::estaAprobado) //Quitar no aprobados 
 		.map(a -> Arrays.stream(a.getNombre().split(" ")) //Obtener Array con todos los elementos del nombre
 				.map(p -> p.substring(0, 1).toUpperCase()) //Obtener iniciales y pasarlas a mayuscula
 				.reduce("", (s1, s2) -> s1 + s2)) //Concatena las iniciales
@@ -76,63 +68,64 @@ public class App {
 		.forEach(ESCRIBIDOR); //Escribe
 
 
-		//Alumnos por curso  --TODO
+		//Alumnos por curso  
+		
+		System.out.println("\nAlumnos por curso: \n");
+		
+		alumnos.stream()
+		.collect(Collectors.groupingBy(Alumno::getCurso))
+		.entrySet()
+		.forEach(ESCRIBIDOR);
 
-		//N.º de alumnos por curso --TODO
+		//N.º de alumnos por curso 
+		
+		System.out.println("\nNº de alumnos por curso: \n");
+		
+		alumnos.stream()
+		.collect(Collectors.groupingBy(Alumno::getCurso, Collectors.counting()))
+		.entrySet()
+		.forEach(ESCRIBIDOR);
 
 		//¿Hay algún alumno en el curso "DAM2"?
-		
-		System.out.println(alumnos.stream().anyMatch(a -> a.getCurso().equalsIgnoreCase("DAM2")) 
+
+		System.out.println(alumnos.stream()
+				.anyMatch(a -> a.getCurso()
+						.equalsIgnoreCase("DAM2")) 
 				? "\nHay alumnos en DAM2." 
 						: "\nNo hay alumnos en DAM2.");
-		
+
 		//Alumno repetidor con peor nota del curso introducido por el teclado, mostrar antes la lista de cursos.
-		
-		
 
-	}
-	
-	private static void mostrarAprobadosCurso(List<Alumno> alumnos) {
-		List<String> cursos, temp;
-		List<Alumno> aprobados;
-		String cual;
-		
-		System.out.println("\nCursos: \n");
-		
-		cursos=(ArrayList<String>) alumnos.stream()
-				.map(Alumno::getCurso) // Recogemos los cursos
-				.distinct() // Quitamos duplicados
-				.collect(Collectors.toList()); // Recogemos en forma de lista
-		
-		cursos.forEach(System.out::println); // Pintamos
-		
-		temp=(ArrayList<String>) cursos.stream()
-				.map(curso -> curso.toUpperCase()) //Transformamos a mayusculas 
-				.collect(Collectors.toList());
-		
-		do {	
-			
-			cual=Teclado.leerString("\nElige un curso: ").toUpperCase();
-				
-		} while (!temp.contains(cual));
-		
-		
-		aprobados=filtrarAlumnosPorCurso(cual,alumnos);
-		
-		System.out.println("\nAlumnos aprobados de "+cual+" :\n");
-		
-		aprobados.forEach(System.out::println);
-		
-	}
+		cursos=alumnos.stream()
+				.map(Alumno::getCurso)
+				.distinct()
+				.collect(Collectors.toSet());
 
-	private static List<Alumno> filtrarAlumnosPorCurso(String cual, List<Alumno> alumnos) {
-		List<Alumno> aprobados;
-		
-		aprobados=alumnos.stream()
-	            .filter(alumno -> alumno.getCurso().equalsIgnoreCase(cual) && alumno.getNota() >= 5)
-	            .collect(Collectors.toList());
-		
-		return aprobados;
+		do {
+			cursos.forEach(ESCRIBIDOR);
+
+			curso=Teclado.leerString("\nQue curso ?");
+		} while (!cursos.contains(curso));
+
+		String cursoActual=curso;
+
+		alumnos.stream()
+		.filter(a->a.getCurso()
+				.equalsIgnoreCase(cursoActual))
+		.filter(Alumno::isRepetidor)
+		.min((a1,a2)->Float
+				.compare(a1.getNota(), a2.getNota()))
+		.ifPresentOrElse(ESCRIBIDOR, ()->System.out.println("\nSin datos"));
+
+		//Nota media de los alumnos del curso con más alumnos. A medias
+		DoubleSummaryStatistics summary= alumnos.stream()
+				.filter(a->a.getCurso()
+						.equalsIgnoreCase(cursoActual))
+				.mapToDouble(Alumno::getNota)
+				.summaryStatistics();
+
+		System.out.println(summary.getAverage());
+
 	}
 
 	public static List<Alumno> cargarAlumnos ()
