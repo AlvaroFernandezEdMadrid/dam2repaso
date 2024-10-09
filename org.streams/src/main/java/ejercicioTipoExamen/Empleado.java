053ad6f5c46aa8537d386514e55ca74804c12fd7
Alvaro Fernandez Lopez <alvaro.fernandez223@educa.madrid.org>
2024-10-09 21:04:34 +0200

    Miercoles clase

--- org.streams/src/main/java/ejercicioTipoExamen/Empleado.java
+++ org.streams/src/main/java/ejercicioTipoExamen/Empleado.java
@@ -1,5 +1,18 @@
 package ejercicioTipoExamen;
 
 public class Empleado {
+	/*
+	 * Se desea acceder a los datos de una BBDD de institutos. 
+	 * Se conoce que de cada instituto se guarda su nombre, 
+	 * su código de centro, su número de teléfono y presupuesto. 
+	 * Se distinguen los institutos por su código de centro. 
+	 * Además se guarda información sobre las personas que trabajan y estudian en él.
 
+
+	De cada persona se guarda sus teléfonos de contacto (compañía de telecomunicaciones, 
+	número de teléfono y sistema operativo), junto a estos datos también se guarda 
+	información sobre su nombre, NIF ,fecha de nacimiento y en el caso de que te 
+	tengan vehículo se guarda su modelo, su color y su matrícula. 
+	Se distinguen las personas por su NIF.
+	 */
 }
