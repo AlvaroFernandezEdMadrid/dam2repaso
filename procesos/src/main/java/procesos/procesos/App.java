b47512d3405306e8023dfb8dbcc184a25d6ab3f9
Alvaro Fernandez Lopez <alvaro.fernandez223@educa.madrid.org>
2024-10-16 21:01:05 +0200

    miercoles

---
+++ procesos/src/main/java/procesos/procesos/App.java
@@ -0,0 +1,13 @@
+package procesos.procesos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
